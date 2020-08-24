06fee6c6a7ee447dcbcd0a87cb40e0b0a10f396c
Mathias <mathias@mathiasfuchs.de>
2020-08-24 16:22:11 +0200

    indentation

--- exampleExec/main.cpp
+++ exampleExec/main.cpp
@@ -2,69 +2,67 @@
 #include <geometry/zGraph.h>
 #include "structural.h"
 
-int main() {
+int main()
+{
 
-// define the connectivity graph as a z graph
-zSpace::zGraph graph;
+    // define the connectivity graph as a z graph
+    zSpace::zGraph graph;
 
-zSpace::zPointArray positions = {
-    zSpace::zVector(0, 0, 0),
-    zSpace::zVector(1, 0, 0),
-    zSpace::zVector(1, 1, 0),
-    zSpace::zVector(0, 1, 0),
-    zSpace::zVector(0.5, 0.5, 0.3)
-};
+    zSpace::zPointArray positions = {
+        zSpace::zVector(0, 0, 0),
+        zSpace::zVector(1, 0, 0),
+        zSpace::zVector(1, 1, 0),
+        zSpace::zVector(0, 1, 0),
+        zSpace::zVector(0.5, 0.5, 0.3)};
 
-// the edge connectivity of a tetrahedron
-zSpace::zIntArray edgeConnects = {0, 1, 1, 2, 2, 3, 3, 0, 0, 4, 1, 4, 2, 4, 3, 4};
+    // the edge connectivity of a tetrahedron
+    zSpace::zIntArray edgeConnects = {0, 1, 1, 2, 2, 3, 3, 0, 0, 4, 1, 4, 2, 4, 3, 4};
 
-graph.create(positions, edgeConnects);
+    graph.create(positions, edgeConnects);
 
-// define the first four nodes to have reactions, defined a simple load on the fifth node
+    // define the first four nodes to have reactions, defined a simple load on the fifth node
 
-structural::Input input;    
-int counter = 0;    
-for (auto p: positions)
-    input.nodes.push_back(structural::Node(p.x, p.y, p.z, 0, counter++));
+    structural::Input input;
+    int counter = 0;
+    for (auto p : positions)
+        input.nodes.push_back(structural::Node(p.x, p.y, p.z, 0, counter++));
 
-// assume the first four nodes are supported
-for (int i = 0; i < 4; i++)
-input.reactions.push_back(structural::NodeReaction(
-    i,
-    structural::NodeReaction::fixed,    
-    structural::NodeReaction::fixed, 
-    structural::NodeReaction::fixed, 
-    structural::NodeReaction::fixed,
-    structural::NodeReaction::fixed, 
-    structural::NodeReaction::fixed
-));
+    // assume the first four nodes are supported
+    for (int i = 0; i < 4; i++)
+        input.reactions.push_back(structural::NodeReaction(
+            i,
+            structural::NodeReaction::fixed,
+            structural::NodeReaction::fixed,
+            structural::NodeReaction::fixed,
+            structural::NodeReaction::fixed,
+            structural::NodeReaction::fixed,
+            structural::NodeReaction::fixed));
 
-// take the connectivity from the zGraph
-for (int i = 0; i < edgeConnects.size()/2; i++)
-    input.frameElements.push_back(structural::FrameElement(edgeConnects[2 * i], edgeConnects[2 * i + 1]));
+    // take the connectivity from the zGraph
+    for (int i = 0; i < edgeConnects.size() / 2; i++)
+        input.frameElements.push_back(structural::FrameElement(edgeConnects[2 * i], edgeConnects[2 * i + 1]));
 
-// apply one load to the fifth element:
-structural::Load load1(0, 0, -10.0, 4);
+    // apply one load to the fifth element:
+    structural::Load load1(0, 0, -10.0, 4);
 
-// define just one load case
-structural::LoadCase loadCase(input, {load1});
+    // define just one load case
+    structural::LoadCase loadCase(input, {load1});
 
-// perform the structural analysis
-structural::LoadCaseResult result = structural::analyze(input, {loadCase});
+    // perform the structural analysis
+    structural::LoadCaseResult result = structural::analyze(input, {loadCase});
 
-// get the result by a function like the following
-auto displacements = result.displacements;
+    // get the result by a function like the following
+    auto displacements = result.displacements;
 
-// copy construct a new graph
-auto displacedGraph = graph;
+    // copy construct a new graph
+    auto displacedGraph = graph;
 
-// read the displacements out of the load result;
-int i = 0;
-for (auto d : displacements) 
-    displacedGraph.vertexPositions[i++] = zSpace::zVector(d.dx, d.dy, d.dz);
+    // read the displacements out of the load result;
+    int i = 0;
+    for (auto d : displacements)
+        displacedGraph.vertexPositions[i++] = zSpace::zVector(d.dx, d.dy, d.dz);
 
+    // similary, obtain axial strains etc
 
-// similary, obtain axial strains etc
-
-return 0;
+    return 0;
 }
