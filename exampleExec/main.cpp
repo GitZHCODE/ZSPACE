5bba1828832991a0ad0043d281ceeda2de7b454a
Mathias <mathias@mathiasfuchs.de>
2020-08-24 16:07:52 +0200

    sample main.cpp for structural analysis, compiles already up to linking

--- exampleExec/main.cpp
+++ exampleExec/main.cpp
@@ -1,15 +1,13 @@
 #include <zCore.h>
 #include <geometry/zGraph.h>
+#include "structural.h"
 
-// here: include the public header file of the structural API, for dummy purposes
-
-
-void main() {
+int main() {
 
 // define the connectivity graph as a z graph
 zSpace::zGraph graph;
 
-zSpace::zPointArray _positions = {
+zSpace::zPointArray positions = {
     zSpace::zVector(0, 0, 0),
     zSpace::zVector(1, 0, 0),
     zSpace::zVector(1, 1, 0),
@@ -17,25 +15,56 @@ zSpace::zPointArray _positions = {
     zSpace::zVector(0.5, 0.5, 0.3)
 };
 
-
 // the edge connectivity of a tetrahedron
-zSpace::zIntArray _edgeConnects = {0, 1, 1, 2, 2, 3, 3, 0, 0, 4, 1, 4, 2, 4, 3, 4};
+zSpace::zIntArray edgeConnects = {0, 1, 1, 2, 2, 3, 3, 0, 0, 4, 1, 4, 2, 4, 3, 4};
 
 graph.create(positions, edgeConnects);
 
-void* model;
-
 // define the first four nodes to have reactions, defined a simple load on the fifth node
 
-// get the geometry from the zGraph
-model->FromZGraph(graph);
+structural::Input input;    
+int counter = 0;    
+for (auto p: positions)
+    input.nodes.push_back(structural::Node(p.x, p.y, p.z, 0, counter++));
 
-// perform the structural analysis as described in the specification
+// assume the first four nodes are supported
+for (int i = 0; i < 4; i++)
+input.reactions.push_back(structural::NodeReaction(
+    i,
+    structural::NodeReaction::fixed,    
+    structural::NodeReaction::fixed, 
+    structural::NodeReaction::fixed, 
+    structural::NodeReaction::fixed,
+    structural::NodeReaction::fixed, 
+    structural::NodeReaction::fixed
+));
+
+// take the connectivity from the zGraph
+for (int i = 0; i < edgeConnects.size()/2; i++)
+    input.frameElements.push_back(structural::FrameElement(edgeConnects[2 * i], edgeConnects[2 * i + 1]));
+
+// apply one load to the fifth element:
+structural::Load load1(0, 0, -10.0, 4);
+
+// define just one load case
+structural::LoadCase loadCase(input, {load1});
+
+// perform the structural analysis
+structural::LoadCaseResult result = structural::analyze(input, {loadCase});
 
 // get the result by a function like the following
-zSpace::zPointArray = model->displacements.ToZPointArray();
+auto displacements = result.displacements;
 
-// similarly, for axial strains etc
+// copy construct a new graph
+auto displacedGraph = graph;
+
+// read the displacements out of the load result;
+int i = 0;
+for (auto d : displacements) 
+    displacedGraph.vertexPositions[i++] = zSpace::zVector(d.dx, d.dy, d.dz);
 
 
+// similary, obtain axial strains etc
+
+return 0;
 }
---
+++ exampleExec/structural.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <vector>  
+  
+namespace structural {
+  
+  class Node {
+    double x;
+    double y;
+    double z;
+    double radius;
+    int id;
+    public:
+     Node(double, double, double, double, int);
+  };
+  class NodeReaction {
+    public:     
+          enum ReactionType { fixed, free };
+          int nodeid;
+    NodeReaction(int, ReactionType, ReactionType, ReactionType, ReactionType, ReactionType, ReactionType);
+    private:
+    ReactionType x;
+    ReactionType y;
+    ReactionType z;
+    ReactionType xx;
+    ReactionType yy;
+    ReactionType zz;
+  };
+  class FrameElement{
+    int node1;
+    int node2;
+   // and some fields for cross section areas and material properties
+   public:
+   FrameElement(int, int);
+  };
+  class Load {
+    double Fx;
+    double Fy;
+    double Fz;
+    int id;
+    public:
+    Load(double, double, double, int);
+  };
+
+  // the geometrical input
+  class Input {
+      public:
+    std::vector<Node> nodes;
+    std::vector<NodeReaction> reactions;
+    std::vector<FrameElement> frameElements;
+  };
+  class LoadCase {
+    const Input* input;
+    // the following can also be empty, in which case the load is just due to self-weight
+    std::vector<Load> appliedLoads;
+    public:
+    LoadCase(const Input& input, const std::vector<Load>& appliedLoads);
+  };
+  // the result
+  class Displacement {
+      public:
+    double dx;
+    double dy;
+    double dz;
+  };
+  class LoadCaseResult {
+      public:
+    const LoadCase* loadCase;
+    std::vector<Displacement> displacements;
+  };
+
+
+  static LoadCaseResult analyze(const Input& input, const std::vector<LoadCase> loadcases);
+  }
